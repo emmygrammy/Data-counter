896fcd42412165ccac37dfc8419baf32daf1f9c0
emmy grammy <ojeabuo24@gmail.com>
2026-01-08 00:42:52 +0100

    App.js

--- src/App.js
+++ src/App.js
@@ -6,31 +6,31 @@ import { useState } from 'react';
 
 function App() {
  const [step, setStep] = useState(1);
-//  const [count, setCount] = useState(0);
+ const [count, setCount] = useState(0);
 
+ const baseDate = new Date(2027, 6, 17 + count);
 
-function increaseStepClick() {
-  setStep(step + 1);
-}
-
-function decreaseStepClick() {
-  setStep(step - 1);
-}
   return (
     <>
     <div className="App">
          <br />
-      <button onClick={decreaseStepClick}>-</button>
+      <button onClick={()=>setStep((s)=>s- 1)}>-</button>
       <span>step: {step}</span>
-      <button onClick={increaseStepClick}>+</button>
+      <button onClick={()=>setStep((s)=>s+ 1)}>+</button>
       <br /> <br />
 
       {/* // count  */}
       <div>
-      <button>-</button>
-      <span>count: 1</span>
-      <button>+</button>
+      <button onClick={()=>setCount(count-step)}>-</button>
+      <span>count: {count}</span>
+      <button onClick={()=>setCount(count+step)}>+</button>
       </div>
+      <br />
+
+      {/* // countDate is 17th June 2027 */}
+      <span>{count} from today is {baseDate.toLocaleDateString()}</span>
+
+
 
     </div>
     </>
